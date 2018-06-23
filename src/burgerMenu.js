14326023a778c73a1d39be8e28477dcb3af33479
Sarah <sarahwbcodes@gmail.com>
2018-06-23 06:06:32 +0300

    switched the animation attribute with marker clicks
    just have to figure out how to tie it to said marker lol

--- src/burgerMenu.js
+++ src/burgerMenu.js
@@ -27,10 +27,10 @@ class Menu extends React.Component {
   render () {
     return (
       <Menu>
-        <a id="PSU" className="menu-item" animation={this.props.google.animation.BOUNCE} href="/">Prince Sultan University</a>
-        <a id="AU" className="menu-item"animation={this.props.google.animation.BOUNCE} href="/about">AlFaisal University</a>
-        <a id="KSU" className="menu-item" animation={this.props.google.animation.BOUNCE}href="/contact">King Saud University</a>
-        <a id="YU" className="menu-item" href="/contact">AlYamamah University</a>
+        <a id="PSU" className="menu-item" onClick={this.onMarkerClick} href="/">Prince Sultan University</a>
+        <a id="AU" className="menu-item" onClick={this.onMarkerClick} href="/about">AlFaisal University</a>
+        <a id="KSU" className="menu-item" onClick={this.onMarkerClick} href="/contact">King Saud University</a>
+        <a id="YU" className="menu-item" onClick={this.onMarkerClick} href="/contact">AlYamamah University</a>
         <a id="PNU" className="menu-item" onClick={this.onMarkerClick} href="/contact">Princess Noura University</a>
         <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
       </Menu>
