d2e4687cc2c08cc8be02bce5ba4d4c98fa7f5402
Sarah <sarahwbcodes@gmail.com>
2018-06-28 14:27:46 +0300

    switched the api for one that fetches custom places list details.

--- src/universities.js
+++ src/universities.js
@@ -1,12 +1,12 @@
 
 //fetching API 
-export const getAll = (lat,lng) =>
-  fetch(`https://api.foursquare.com/v2/venues/search?ll=${lat},${lng}&categoryId=4bf58dd8d48988d1ae941735&client_id=ASZFT4LNLRCNQQJ1EE5LWPF1HRQCUN5XLF3M4ALZ20DWKOD1&client_secret=PK1NNHTEOTVZKLBFZAKSYHXJJHLJN3MBOCDSZG0MP5AGI35A&v=20180723`)
+export const getAll = () =>
+  fetch(`https://api.foursquare.com/v2/lists/499268126/universities?&client_id=ASZFT4LNLRCNQQJ1EE5LWPF1HRQCUN5XLF3M4ALZ20DWKOD1&client_secret=PK1NNHTEOTVZKLBFZAKSYHXJJHLJN3MBOCDSZG0MP5AGI35A&v=20180723&v=20180628`)
   .then(res =>  { return res.json()}).catch(err => {window.alert(err)})
   
-  
-  export const apiSearch = (lat, lng)=>{
-    fetch(`https://api.foursquare.com/v2/venues/search?ll=${lat},${lng}&categoryId=4bf58dd8d48988d1ae941735&client_id=ASZFT4LNLRCNQQJ1EE5LWPF1HRQCUN5XLF3M4ALZ20DWKOD1&client_secret=PK1NNHTEOTVZKLBFZAKSYHXJJHLJN3MBOCDSZG0MP5AGI35A&v=20180723`)
+ 
+  export const apiSearch = ()=>{
+    fetch(`https://api.foursquare.com/v2/lists/499268126/universities?&client_id=ASZFT4LNLRCNQQJ1EE5LWPF1HRQCUN5XLF3M4ALZ20DWKOD1&client_secret=PK1NNHTEOTVZKLBFZAKSYHXJJHLJN3MBOCDSZG0MP5AGI35A&v=20180723&v=20180628`)
    .then(res => res.json())
    .then(data =>{
      this.setState({
