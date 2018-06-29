56f025d1343715d2f419744fd66fc0ba1d1a524c
Sarah <sarahwbcodes@gmail.com>
2018-06-29 06:17:16 +0300

    list parameter

--- src/universityList.js
+++ src/universityList.js
@@ -7,15 +7,15 @@ class universityList extends Component {
     this.state ={
     // I create the list to know the value of the clicked list and send it to the map Component
     lists: [
-      "AlFaisal University", 
-      "AlYamamah University", 
-      "King Saud University", 
-      "Princess Noura Bint Abdulrahman University", 
-      "Prince Sultan University"
+      {name:"AlFaisal University" , position: {lat: 24.664930700770164, lng: 46.67787865440406}},
+      {name: "AlYamamah University", position: {lat: 24.86366401158759, lng: 46.59419775009155}},
+      {name: "King Saud University", position: {lat: 24.724761378957492, lng: 46.62260004907829 } },
+      {name: "Princess Noura Bint Abdulrahman University", position: {lat: 24.85076709581032, lng: 46.72084095898374}},
+      {name:"Prince Sultan University", position: {lat: 24.73549191880903, lng: 46.6995250461035}}
       ],
       query:''
   }}
-  
+
  uniUpdate = (query, lat, lng, data) => {
     this.setState({
       query: data
@@ -39,10 +39,10 @@ render(){
     <input type="text" value={this.state.query} onChange={this.uniUpdate.bind(this)}/>
       </div>
       <ul className="map-list">
-        {this.state.lists.map((name) =>(
-          <li key= {name} tabIndex="0">
-            <button onClick={()=>this.props.showInfo(name)}  className='clickable-places'>
-              {name}
+        {this.state.lists.map((list) =>(
+          <li key= {list.name} tabIndex="0">
+            <button onClick={()=>this.props.showInfo(list)}  className='clickable-places'>
+              {list.name}
             </button></li>
           ))}
         </ul>
