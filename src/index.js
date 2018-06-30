f5051a6452fe4bd1c51502fae8301688da60e996
Sarah <sarahwbcodes@gmail.com>
2018-06-30 06:23:39 +0300

    in attempt to fix errors, imported googlemaps package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+
+//all outsourced code was referenced
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import GoogleMapReact from 'google-maps-react';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
