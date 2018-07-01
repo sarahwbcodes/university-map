7cae8b0566d07bf51b692abf27e73ed379b527fd
Sarah <sarahwbcodes@gmail.com>
2018-07-01 22:19:02 +0300

    refactored for filtering functionality

--- src/mapComponent.js
+++ src/mapComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import * as API from './universities'
 import List from './universityList'
+import escapeRegExp from 'escape-string-regexp'
 //import GoogleMapReact from 'google-map-react';
 //courtesy of the Google Maps React package, this is the starter code from the packages + some personal tweaks
 export class MapComponent extends Component {
@@ -11,8 +12,10 @@ constructor(){
     showingInfoWindow: false,
     activeMarker: {},
     selectedPlace: '',
-    position: {},
+    position: {lat: 24.713, lng:46.6753 },
    Universities:[],
+   query: '',
+   queryResult:[],
    noMapError: true
   };
 }
@@ -59,42 +62,52 @@ showInfo = (list)=>{
       showingInfoWindow: true
     });
   }
-  
-  const loadGoogleMapsApi = require('load-google-maps-api')
 
-  loadGoogleMapsApi().then(function (googleMaps) {
-    new googleMaps.Map(document.querySelector('.map'), {
-      center: {
-        lat: 24.7136,
-        lng: 46.6753
-      },
-      zoom: 10
-    })
-  }).catch(function (error) {
-    window.alert(error)
-  })
+  listUpdate=(query)=>{
+  this.setState({query: query.trim()})
+}
+
 
   render() {
+    let uniResults
+  if (this.state.query){
+    const result = new RegExp(escapeRegExp(this.state.query),'i')
+  uniResults = this.state.Universities.filter((list) => result.test(list.venue.name))
+}else {
+  uniResults = this.state.Universities
+}
       return (
         <div>
+              <div>
+    <input role="search" 
+    tabIndex="0"
+    placeholder="Enter University"
+    aria-label="a way to filter the list items" 
+    className="map-filter"
+    type="text" value={this.state.query}
+    onChange={(event)=> this.listUpdate(event.target.value)}/>
+      </div>
           <List
-            showInfo= {this.showInfo}/>
-          {this.state.noMapError? (<Map
+            showInfo= {this.showInfo}
+            List={uniResults}
+            />
+          {this.state.noMapError? (
+          <Map
             google={this.props.google}
             initialCenter={{
-              lat: 24.7136,
-              lng: 46.6753
+              lat: parseFloat(24.7136),
+              lng: parseFloat(46.6753)
             }}
             zoom={10}
             onClick={this.onMapClicked}
             style={{width: '100%', height: '85%', position: 'absolute'}}
             className={'map'}
             >
-            {
-              this.state.Universities.map((uni)=>
-              <Marker key={uni.venue.name}
+            {console.log(uniResults)}{
+              uniResults.map((uni)=>
+              <Marker key={uni.venue.id}
                 name={uni.venue.name}
-                position={{lat:uni.venue.location.lat, lng:uni.venue.location.lng}}
+                position={{lat: parseFloat(uni.venue.location.lat), lng: parseFloat(uni.venue.location.lng)}}
                 onClick={this.onMarkerClick}
                 animation={this.props.google.maps.Animation.DROP}
                 />
--- src/universityList.js
+++ src/universityList.js
@@ -1,19 +1,11 @@
 import React, {Component} from 'react'
 import * as API from './universities'
-import escapeRegExp from 'escape-string-regexp'
 
 class universityList extends Component {
   constructor(){
     super();
     this.state ={
     // I create the list to know the value of the clicked list and send it to the map Component
-    lists: [
-      {name:"AlFaisal University" , position: {lat: 24.664930700770164, lng: 46.67787865440406}},
-      {name: "AlYamamah University", position: {lat: 24.86366401158759, lng: 46.59419775009155}},
-      {name: "King Saud University", position: {lat: 24.724761378957492, lng: 46.62260004907829 } },
-      {name: "Princess Noura Bint Abdulrahman University", position: {lat: 24.85076709581032, lng: 46.72084095898374}},
-      {name:"Prince Sultan University", position: {lat: 24.73549191880903, lng: 46.6995250461035}}
-      ],
       query:''
   }}
 //Udacity's controlled component lessons are the guide
@@ -22,25 +14,13 @@ listUpdate=(query)=>{
 }
 
 render(){
-  let uniResults
-  if (this.state.query){
-    const result = new RegExp(escapeRegExp(this.state.query),'i')
-  uniResults = this.state.lists.filter((list) => result.test(list.name))
-}else {
-  uniResults = this.state.lists
-}
   return(
     <div className="map-main" role="main">
-    <div>
-    <input role="search" aria-label="a way to filter the list items" className="map-filter"
-    type="text" value={this.state.query}
-    onChange={(event)=> this.listUpdate(event.target.value)}/>
-      </div>
       <ul className="map-list">
-        {uniResults.map((list) =>(
-          <li key= {list.name} tabIndex="0">
+        {this.props.List && this.props.List.map((list) =>(
+          <li key= {list.venue.name} tabIndex="0">
             <button role="button" onClick={()=>this.props.showInfo(list)}  className='clickable-places'>
-              {list.name}
+              {list.venue.name}
             </button></li>
           ))}
         </ul>
